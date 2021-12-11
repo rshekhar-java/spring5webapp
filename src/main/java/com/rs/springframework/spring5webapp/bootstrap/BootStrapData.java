5da3b2025a0fbe04ab2713e623ca191017451a40
Ravi Shekhar <rsshekhar27@outlook.com>
2021-12-10 21:18:55 -0600

    Adding Publisher Relationship

--- src/main/java/com/rs/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/com/rs/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package com.rs.springframework.spring5webapp.bootstrap;
 
 import com.rs.springframework.spring5webapp.domain.Author;
 import com.rs.springframework.spring5webapp.domain.Book;
+import com.rs.springframework.spring5webapp.domain.Publisher;
 import com.rs.springframework.spring5webapp.repositories.AuthorRepository;
 import com.rs.springframework.spring5webapp.repositories.BookRepository;
+import com.rs.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,14 +14,27 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        System.out.println("Started in Bootstrap");
+
+        Publisher publisher = new Publisher();
+        publisher.setName("SFG Publishing");
+        publisher.setAddressLine1("121 McDowel Blvd,Suite 102");
+        publisher.setCity("St. Petersburg");
+        publisher.setState("FL");
+        publisher.setZip("50045");
+
+        publisherRepository.save(publisher);
+
         Author eric = new Author("Eric","Evans");
         Book ddd = new Book("Domain Driven Design","123123");
         eric.getBooks().add(ddd);
@@ -38,8 +53,9 @@ public class BootStrapData implements CommandLineRunner {
         authorRepository.save(rod);
         bookRepository.save(noEJB);
 
-        System.out.println("Started in Bootstrap");
+
         System.out.println("Number of Books: "+bookRepository.count());
         System.out.println("Number of Authors: "+authorRepository.count());
+        System.out.println("Number of Publisher: "+publisherRepository.count());
     }
 }
